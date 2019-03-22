fc6563eb4d9d8f03858296f2d673883f32012924
maobui <mao.buidinh@hotmail.com>
2019-03-22 14:33:38 +0700

    - Start.

--- app/src/main/java/com/me/bui/photouploader/MainActivity.kt
+++ app/src/main/java/com/me/bui/photouploader/MainActivity.kt
@@ -1,12 +1,98 @@
 package com.me.bui.photouploader
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  companion object {
+    private const val GALLERY_REQUEST_CODE = 300
+    private const val PERMISSIONS_REQUEST_CODE = 301
+
+    private const val MAX_NUMBER_REQUEST_PERMISSIONS = 2
+
+    private const val IMAGE_TYPE = "image/*"
+    private const val IMAGE_CHOOSER_TITLE = "Select Picture"
+
+    private val PERMISSIONS = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+  }
+
+  private var permissionRequestCount = 0
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    initUi()
+
+    requestPermissionsIfNecessary()
+  }
+
+  private fun initUi() {
+    uploadGroup.visibility = View.GONE
+
+    pickPhotosButton.setOnClickListener { showPhotoPicker() }
+  }
+
+  private fun showPhotoPicker() {
+    val intent = Intent().apply {
+      type = IMAGE_TYPE
+      putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+      action = Intent.ACTION_OPEN_DOCUMENT
     }
+
+    startActivityForResult(Intent.createChooser(intent, IMAGE_CHOOSER_TITLE), GALLERY_REQUEST_CODE)
+  }
+
+  private fun requestPermissionsIfNecessary() {
+    if (!hasRequiredPermissions()) {
+      askForPermissions()
+    }
+  }
+
+  private fun askForPermissions() {
+    if (permissionRequestCount < MAX_NUMBER_REQUEST_PERMISSIONS) {
+      permissionRequestCount += 1
+
+        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSIONS_REQUEST_CODE)
+    } else {
+      pickPhotosButton.isEnabled = false
+    }
+  }
+
+  private fun hasRequiredPermissions(): Boolean {
+    val permissionResults = PERMISSIONS.map { permission ->
+      ContextCompat.checkSelfPermission(
+          this,
+          permission
+      ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    return permissionResults.all { isGranted -> isGranted }
+  }
+
+  override fun onRequestPermissionsResult(
+      code: Int,
+      permissions: Array<String>,
+      result: IntArray) {
+    super.onRequestPermissionsResult(code, permissions, result)
+    if (code == PERMISSIONS_REQUEST_CODE) {
+      requestPermissionsIfNecessary()
+    }
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    if (data != null && resultCode == Activity.RESULT_OK && requestCode == GALLERY_REQUEST_CODE) {
+    }
+  }
 }
